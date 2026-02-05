f4898dd78c4cbd17c19fa96827ed2c3210605edb
Minsun Kim <violetsunshades@snu.ac.kr>
2026-02-06 02:44:07 +0800

    Add Navis chatbot skeleton

---
+++ src/main/java/Navis.java
@@ -0,0 +1,17 @@
+public class Navis {
+    public static void main(String[] args) {
+
+        String logo =
+                "N   N   AAAAA   V   V   IIIII   SSSSS\n"
+                        + "NN  N   A   A   V   V     I    S    \n"
+                        + "N N N   AAAAA    V V      I     SSSS \n"
+                        + "N  NN   A   A     V       I        S \n"
+                        + "N   N   A   A     V     IIIII   SSSSS\n";
+
+        System.out.println("Hello! I'm Navis\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________");
+    }
+}
