b0add6198b0986a58d267a19771f399f21c4720a
Minsun Kim <violetsunshades@snu.ac.kr>
2026-02-06 13:17:39 +0800

    Add support for echo commands

--- src/main/java/Navis.java
+++ src/main/java/Navis.java
@@ -1,6 +1,9 @@
-public class Navis {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Navis {
+    private static final String LINE = "____________________________________________________________";
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo =
                 "N   N   AAAAA   V   V   IIIII   SSSSS\n"
                         + "NN  N   A   A   V   V     I    S    \n"
@@ -8,10 +11,26 @@ public class Navis {
                         + "N  NN   A   A     V       I        S \n"
                         + "N   N   A   A     V     IIIII   SSSSS\n";
 
+        System.out.println(LINE);
         System.out.println("Hello! I'm Navis\n" + logo);
         System.out.println("What can I do for you?");
-        System.out.println("______________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("________________________________");
+        System.out.println(LINE);
+
+        while(true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(LINE);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+
+            System.out.println(LINE);
+            System.out.println(" "+input);
+            System.out.println(LINE);
+        }
+
+        scanner.close();
     }
 }
