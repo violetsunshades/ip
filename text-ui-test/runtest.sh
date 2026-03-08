743e5c33b8a09cae51af765d61798f408069be2f
Minsun Kim <violetsunshades@snu.ac.kr>
2026-03-08 13:41:27 +0800

    Add text UI texts using input output redirection

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -19,15 +19,11 @@ then
     exit 1
 fi
 
-# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
-
-# convert to UNIX format
-cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+# run the program, feed commands from input.txt file and redirect the output to ACTUAL.TXT
+java -classpath ../bin Navis < input.txt > ACTUAL.TXT
 
 # compare the output to the expected output
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
+diff ACTUAL.TXT EXPECTED.TXT
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
